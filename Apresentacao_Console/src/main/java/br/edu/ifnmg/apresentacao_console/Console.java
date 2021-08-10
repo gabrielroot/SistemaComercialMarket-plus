39ff56e41d38f18a549c707de1844ad9e5fb6ef8
gabrielroot <gabrielfer.s88@gmail.com>
2021-08-10 19:59:51 -0300

    Inicializada a estrurura da persistência && Criação de atributos base em Pessoa

---
+++ Apresentacao_Console/src/main/java/br/edu/ifnmg/apresentacao_console/Console.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.apresentacao_console;
+
+import br.edu.ifnmg.logicaAplicacao.Pessoa;
+import br.edu.ifnmg.logicaAplicacao.PessoaRepositorio;
+import br.edu.ifnmg.logicaAplicacao.TipoDocumento;
+import br.edu.ifnmg.logicaAplicacao.TipoPessoa;
+import br.edu.ifnmg.persistencia.PessoaDAO;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Console {
+     public static void main(String[] args){
+        PessoaRepositorio repo = new PessoaDAO();
+        
+//        Usuario user = repo.Abrir(201L);
+//        System.out.println("LOGIN="+user.getLogin());
+        
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Pessoa01");
+        pessoa.setTipoPessoa(TipoPessoa.Juridica);
+        pessoa.setTipoDocumento(TipoDocumento.CertidaoNascimento);
+        pessoa.setNumeroDocumento("123424");
+        System.out.println(pessoa);
+        if(repo.Salvar(pessoa)){
+            System.out.println("Sucesso!!");
+        }else{
+            System.out.println("Falha!!");
+        }
+        
+//        repo.Apagar(user);
+    }
+}
---
+++ LogicaAplicacao/src/main/java/br/edu/ifnmg/logicaAplicacao/TipoDocumento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.logicaAplicacao;
+
+/**
+ *
+ * @author gabriel
+ */
+public enum TipoDocumento {
+    RG,
+    CNPJ,
+    CTPS,
+    CNH,
+    Passaporte,
+    CertidaoNascimento,
+    CertidaoCasamento,
+    CertidaoProntuario,
+    CarteiraMilitar
+}

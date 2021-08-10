1041cb89f0e1066b7bd9a7fdf877977f8b029c6b
kesleymartins <kesley.martins@vivaldi.net>
2021-08-09 21:02:13 -0300

    Adicionado nome da tabela pessoa

--- LogicaAplicacao/src/main/java/br/edu/ifnmg/logicaAplicacao/Pessoa.java
+++ LogicaAplicacao/src/main/java/br/edu/ifnmg/logicaAplicacao/Pessoa.java
@@ -11,12 +11,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
- * @author gabriel
+ * @author gabriel, kesley
  */
 @Entity
+@Table(name="pessoa")
 public class Pessoa implements Serializable {
 
     private static final long serialVersionUID = 1L;

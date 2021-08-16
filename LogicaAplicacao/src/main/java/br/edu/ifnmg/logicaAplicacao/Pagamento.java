a527970158c71a9a4dabf5a6ea9cb32d8340c373
gabrielroot <gabrielfer.s88@gmail.com>
2021-08-16 19:14:02 -0300

    inhetitance

--- LogicaAplicacao/src/main/java/br/edu/ifnmg/logicaAplicacao/Pagamento.java
+++ LogicaAplicacao/src/main/java/br/edu/ifnmg/logicaAplicacao/Pagamento.java
@@ -10,12 +10,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 
 /**
  *
  * @author gabriel
  */
 @Entity
+@Table(name="pagamento")
+@Inheritance(strategy=InheritanceType.JOINED) 
 public class Pagamento implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -23,13 +28,8 @@ public class Pagamento implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     @Override
     public int hashCode() {

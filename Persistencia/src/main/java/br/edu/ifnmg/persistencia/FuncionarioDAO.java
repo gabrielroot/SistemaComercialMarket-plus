d29cdb75b89c096673cd98e1f05050621a658d8d
kesleymartins <kesley.martins@vivaldi.net>
2021-08-16 12:01:04 -0300

    Criacao da classe FuncionarioDAO

---
+++ Persistencia/src/main/java/br/edu/ifnmg/persistencia/FuncionarioDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.persistencia;
+
+import br.edu.ifnmg.logicaAplicacao.Funcionario;
+import br.edu.ifnmg.logicaAplicacao.FuncionarioRepositorio;
+import java.util.List;
+
+/**
+ *
+ * @author kesley
+ */
+public class FuncionarioDAO extends DataAccessObject<Funcionario> implements FuncionarioRepositorio {
+    
+    public FuncionarioDAO() {
+        super(Funcionario.class);
+    }
+
+    @Override
+    public List<Funcionario> Buscar(Funcionario obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

5f2cebb126f7a018598665940111d9b0dc5f3474
kesley <kesley.martins@vivaldi.net>
2021-09-05 13:13:30 -0300

    renomeacao de variaveis

--- Apresentacao_Desktop/src/main/java/br/edu/ifnmg/apresentacao_desktop/TelaInicio.java
+++ Apresentacao_Desktop/src/main/java/br/edu/ifnmg/apresentacao_desktop/TelaInicio.java
@@ -86,10 +86,10 @@ public class TelaInicio extends javax.swing.JInternalFrame {
         jPanel18 = new javax.swing.JPanel();
         jPanel19 = new javax.swing.JPanel();
         jPanel20 = new javax.swing.JPanel();
-        jLabel16 = new javax.swing.JLabel();
+        imgUsuario = new javax.swing.JLabel();
         labelUsuario = new javax.swing.JLabel();
         labelCargoUsuario = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        btnEncerrarSessao = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(243, 230, 230));
 
@@ -757,19 +757,19 @@ public class TelaInicio extends javax.swing.JInternalFrame {
 
         jPanel20.setBackground(new java.awt.Color(107, 70, 70));
 
-        jLabel16.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel16.setIcon(new javax.swing.ImageIcon(getClass().getResource("/user.png"))); // NOI18N
+        imgUsuario.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        imgUsuario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/user.png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel20Layout = new javax.swing.GroupLayout(jPanel20);
         jPanel20.setLayout(jPanel20Layout);
         jPanel20Layout.setHorizontalGroup(
             jPanel20Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel16, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 84, Short.MAX_VALUE)
+            .addComponent(imgUsuario, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 84, Short.MAX_VALUE)
         );
         jPanel20Layout.setVerticalGroup(
             jPanel20Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel20Layout.createSequentialGroup()
-                .addComponent(jLabel16, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(imgUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -806,15 +806,15 @@ public class TelaInicio extends javax.swing.JInternalFrame {
                 .addContainerGap())
         );
 
-        jButton1.setBackground(new java.awt.Color(142, 54, 54));
-        jButton1.setFont(new java.awt.Font("sansserif", 0, 24)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Encerrar Sessão");
-        jButton1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnEncerrarSessao.setBackground(new java.awt.Color(142, 54, 54));
+        btnEncerrarSessao.setFont(new java.awt.Font("sansserif", 0, 24)); // NOI18N
+        btnEncerrarSessao.setForeground(new java.awt.Color(255, 255, 255));
+        btnEncerrarSessao.setText("Encerrar Sessão");
+        btnEncerrarSessao.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        btnEncerrarSessao.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btnEncerrarSessao.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnEncerrarSessaoActionPerformed(evt);
             }
         });
 
@@ -823,14 +823,14 @@ public class TelaInicio extends javax.swing.JInternalFrame {
         jPanel18Layout.setHorizontalGroup(
             jPanel18Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel19, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(btnEncerrarSessao, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel18Layout.setVerticalGroup(
             jPanel18Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel18Layout.createSequentialGroup()
                 .addComponent(jPanel19, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(btnEncerrarSessao, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -869,24 +869,24 @@ public class TelaInicio extends javax.swing.JInternalFrame {
         }
     }//GEN-LAST:event_panelRelatoriosMouseClicked
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnEncerrarSessaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEncerrarSessaoActionPerformed
         this.telaPrincipal.setVisible(false);
         Login login = new Login();
         login.setVisible(true);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnEncerrarSessaoActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnEncerrarSessao;
     private javax.swing.JLabel imgComprar;
     private javax.swing.JLabel imgCrediario;
     private javax.swing.JLabel imgEstoque;
     private javax.swing.JLabel imgFinanceiro;
     private javax.swing.JLabel imgPessoas;
     private javax.swing.JLabel imgRelatorios;
+    private javax.swing.JLabel imgUsuario;
     private javax.swing.JLabel imgVender;
-    private javax.swing.JButton jButton1;
     private javax.swing.JDesktopPane jDesktopPane1;
-    private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel17;
     private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel19;
